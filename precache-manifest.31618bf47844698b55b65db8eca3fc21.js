7d75b3d4b409cb0ac9a353aa10ca250b58b7aab2
James Farfan <jamesrojas710@gmail.com>
2020-08-11 16:54:38 -0400

    Updates

---
+++ precache-manifest.31618bf47844698b55b65db8eca3fc21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596152248aadc82283d79ee72caa5a9b",
+    "url": "/%3Crepo%3E/index.html"
+  },
+  {
+    "revision": "0d3dea15d60c4e9167e7",
+    "url": "/%3Crepo%3E/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d98e559e818129888ea7",
+    "url": "/%3Crepo%3E/static/js/2.76c6d7b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%3Crepo%3E/static/js/2.76c6d7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3dea15d60c4e9167e7",
+    "url": "/%3Crepo%3E/static/js/main.2df2c14d.chunk.js"
+  },
+  {
+    "revision": "b54bb3c7abc5bb3ead46",
+    "url": "/%3Crepo%3E/static/js/runtime-main.484f553f.js"
+  }
+]);
